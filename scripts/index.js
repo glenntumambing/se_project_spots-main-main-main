c6220fec18fc607a399b078d5c1991bc006b9e8a
glenn.tumambing <glenn_aristotle@yahoo.com>
2025-02-19 20:04:21 -0800

    S4

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,35 @@ const intialCards = [
         name: "Mountain house", 
         link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",},
 ] ;
+
+const profileEditButton = document.querySelector(".profile__edit-btn") ;
+const profileName = document.querySelector(".profile__name")
+const profileDescription = document.querySelector(".profile__description")
+
+const editModal = document.querySelector("#edit-modal") ;
+const editFormElement = editModal.querySelector(".modal__form")
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const editModalNameInput = editModal.querySelector("#profile-name-input") ;
+const editModalDescriptionInput = editModal.querySelector("#profile-description-input") ;
+
+
+
+function openModal() {
+    editModalNameInput.value = profileName.textContent;
+    editModalDescriptionInput.value = profileDescription.textContent;
+    editProfileModal.classList.add("modal_opened") ;
+}
+
+function closeModal() {
+    editProfileModal.classList.remove("modal_opened");
+}
+
+function handleEditFormSubmit(evt) {
+    evt.preventDefault();
+    profileDescription.textContent =editModalDescriptionInput.value;
+    profileName.textContent=editModalNameInput.value;
+}
+
+profileEditButton.addEventListener("click", openModal);
+editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit, handleEditFormSubmit");
